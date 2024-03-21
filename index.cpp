0360e809a76e16b9cd4ed03aa00e3d8cbaa7c10b
Bamidele joshua olayinka <Olayinka775@gmail.com>
2024-03-21 12:51:32 +0100

    First time

---
+++ index.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+    std::cout << "Welcome to C++ class. This class promised to be exciting." << std::endl;
+    std::cout << "This is the first class. It started late due to some technical issues \n";
+    return 0;
+}
